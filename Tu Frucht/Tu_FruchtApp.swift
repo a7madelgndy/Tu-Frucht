7bb266600fcc9e00a4a2cf129f2d6f7bf394024a
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-12-25 18:59:56 +0200

    Initial Commit

---
+++ Tu Frucht/Tu_FruchtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tu_FruchtApp.swift
+//  Tu Frucht
+//
+//  Created by Ahmed El Gndy on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tu_FruchtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
